63821e0f3da7cd48a5df1653257dca78522ee1a8
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-07-06 06:50:53 +0100

    added Docker files

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const _ = require("lodash")
-const server = http.createServer((req, res) => {});
-
-server.listen(3000, "localhost", () =>
-  console.log("running we server at port 3000")
-);
